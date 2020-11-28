9c8974de06f70bdd2b5bde5c436d6efeafb23a5a
SangLangThang <sang52ck@gmail.com>
2020-11-28 20:37:17 +0700

    add-map-warp

--- main.js
+++ main.js
@@ -2,19 +2,46 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 const proxy = require("node-tcp-proxy");
 const util = require("util");
-
 const fs = require('fs');
-var myData = JSON.parse(fs.readFileSync('dataWarp.json'));
+var dataWarp="";
+var myClear=0;
+var countSendEnd=0;
+function textext(myData){
+  
+  if(myData.length==13||myData.length==8){
+    myClear++
+    dataWarp+=myData.toString("hex")+"\n";
+    fs.writeFile('logWarp.txt', dataWarp, function (err) {});
+   }
+
+if(myClear>50){
+  dataWarp="";
+    fs.writeFile('logWarp.txt', dataWarp, function (err) {});
+    myClear=0;
+}
+}
+
 var newProxy = proxy.createProxy(6414, "103.48.192.145", 6414,{
   upstream: function(context, data) {
-    //console.log(data)
+    textext(data);
+   // console.log("up")
+   // console.log(data)
     return data;
 },
 
 downstream: function(context, data) {
+  
   if(newProxy.warp=="start"){
     context.serviceSocket.write(Buffer.from(newProxy.textMove,"hex"));
     context.serviceSocket.write(Buffer.from(newProxy.textWarp,"hex"));
+    newProxy.warp="end";
+  }
+  if((newProxy.warp=="talk")&&(countSendEnd!=0)){
+    context.serviceSocket.write(Buffer.from(newProxy.textMove,"hex"));
+    context.serviceSocket.write(Buffer.from(newProxy.textWarp,"hex"));
+    for(let k=0;k<countSendEnd;k++){
+      context.serviceSocket.write(Buffer.from("59e9afadb9ab","hex"));
+    }
     newProxy.warp="end";
   }
     return data;
@@ -46,18 +73,19 @@ app.on('activate', () => {
   }
   
 })
-ipcMain.on("start-warp",function (event, arg) {
-  newProxy.warp="start";
-  newProxy.textMove=data[0];
-  newProxy.textWarp=data[1];
-  event.reply('finish-warp', arg)
-});
 ipcMain.on("addListWarp",function (event, arg) {
-  newProxy.warp="start";
-  newProxy.textMove=arg[0];
-  newProxy.textWarp=arg[1];
-  
-  event.reply('sendListWarp', arg)
+  //console.log(arg)
+  if(arg[0].slice(0,4)=="Talk"){
+    countSendEnd=parseInt(arg[0][4]);
+    newProxy.warp="talk";
+    newProxy.textMove=arg[1][0];
+    newProxy.textWarp=arg[1][1];
+  }else{
+    newProxy.warp="start";
+    newProxy.textMove=arg[1][0];
+    newProxy.textWarp=arg[1][1];
+  }
+  event.reply('sendListWarp', arg[0])
 });
 
 
--- showJsonData.js
+++ showJsonData.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
-let myData = JSON.parse(fs.readFileSync('dataWarp.json'));
 
-let h=Object.keys(myData["Dự Châu"])[0];
-console.log(myData["Dự Châu"][h][0]);
+const data = fs.readFileSync('correctWarp.txt', {encoding:'utf8', flag:'r'}); 
+let lim=data.length;
+let fix=[]
+let text=""
+var line=1;
+for(let i=0;i<lim;i=i+46){
+    fix.push([data.slice(i,i+26),data.slice(i+28,i+44)]);
+}
+for(let j=0;j<fix.length;j++){
+    text+=line + "."+fix[j][0]+ "SANG"+fix[j][1]+"."+"\r"
+    line=line+1
+}
+fs.writeFile('dataOK.txt', text, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+  var my="Talk3:dsdsds"
+  console.log(my[4])
